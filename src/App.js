61600eb2763ebe9994a99a3769733fd7cc895a5b
Matt Bowen <mattbowen03@gmail.com>
2022-08-21 14:29:48 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+import ChoreForm from "./components/choreForm/ChoreForm";
+import ChoreChart from "./components/choreChart/ChoreChart";
 
 function App() {
+  const [choreLogs, setChoreLogs] = useState([]);
+  const addChoreLog = (log) => {
+    let logs = [...choreLogs, log];
+    setChoreLogs(logs);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <section>
+        <ChoreForm addChoreLog={addChoreLog} />
+        <ChoreChart chores={choreLogs} />
+      </section>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'>
           Learn React
         </a>
       </header>
